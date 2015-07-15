a0e6822fadf4157d9ed336526a8e8cebf1dc7d9f
root <root@centos.(none)>
2015-07-15 08:05:51 +0800

    修改测试客户端

--- test/client.js
+++ test/client.js
@@ -1,32 +1,39 @@
 
 var io = require('socket.io-client');
-var host = 'ws://localhost:5002';
+var host = 'ws://localhost:5001';
+var host2 = 'ws://localhost:5002';
 var rehost = 'ws://192.168.41.102:5001';
 var yuhost = 'http://ws.qqshidao.com';
 var uhost = 'http://ws.51winball.com';
-var socket = io.connect(host, {
-    'timeout': 1000,
-    'reconnectionAttempts': 50,
-    forceNew: true,
-});
+var uhost2 = 'ws://123.59.40.113:5002';
 
-socket.on('connect', function(){
-    console.log('connect');
-    socket1.emit('data', 'msg from socket');
-});
-socket.on('disconnect', function(){
-    console.log('disconnect');
-});
-var socket1 = io.connect(host, {
-    'timeout': 1000,
-    'reconnectionAttempts': 50,
-    forceNew: true,
-});
+var length = 5000;
 
-socket1.on('connect', function(){
-    console.log('connect');
-    socket1.emit('data', 'msg from socket1');
-});
-socket1.on('disconnect', function(){
-    console.log('disconnect');
-});
+function bench(){
+    if(length){
+        length--;
+        var socket = io.connect(host2, {
+            'timeout': 1000,
+            'reconnectionAttempts': 50,
+            forceNew: true,
+        });
+
+        /*
+        socket.on('connect', function(){
+            console.log('connect');
+        });
+        */
+        socket.on('reconnect', function(){
+            console.log('reconnection');
+        });
+        socket.on('disconnect', function(){
+            console.log('disconnect');
+        });
+        setTimeout(bench, 5);
+    }
+    else{
+        console.log('done');
+    }
+}
+
+bench();
