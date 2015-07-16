f0e9d9166f2e7ea445db4190785d88533e2e02fb
root <root@centos.(none)>
2015-07-17 05:40:41 +0800

    git说冲突要添加完才能pull

--- test/client.js
+++ test/client.js
@@ -1,28 +1,29 @@
 
 var io = require('socket.io-client');
 var host = 'ws://localhost:5001';
-var host2 = 'ws://localhost:5002';
+var host2 = 'ws://127.0.0.1:5002';
 var rehost = 'ws://192.168.41.102:5001';
 var yuhost = 'http://ws.qqshidao.com';
 var uhost = 'http://ws.51winball.com';
 var uhost2 = 'ws://123.59.40.113:5002';
 
-var length = 5000;
+var length = 1;
 
 function bench(){
     if(length){
         length--;
-        var socket = io.connect(host2, {
+        var socket = io.connect(uhost2, {
             'timeout': 1000,
             'reconnectionAttempts': 50,
             forceNew: true,
+            transport: "websocket",
         });
 
-        /*
+        //*
         socket.on('connect', function(){
             console.log('connect');
         });
-        */
+        //*/
         socket.on('reconnect', function(){
             console.log('reconnection');
         });
